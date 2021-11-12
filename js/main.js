9a58e9a9e2d47b8cb24b69a27dabd66dd1a92301
elepot87 <78643839+elepot87@users.noreply.github.com>
2021-11-12 16:03:48 +0100

    add data in html

--- js/main.js
+++ js/main.js
@@ -7,30 +7,31 @@ const app = new Vue({
   data: {
     slides: [
       {
-        image: "img/01.jpg",
+        image: "../img/01.jpg",
         title: "Svezia",
         text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
       },
       {
-        image: "img/02.jpg",
+        image: "../img/02.jpg",
         title: "Svizzera",
         text: "Lorem ipsum.",
       },
       {
-        image: "img/03.jpg",
+        image: "../img/03.jpg",
         title: "Gran Bretagna",
         text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
       },
       {
-        image: "img/04.jpg",
+        image: "../img/04.jpg",
         title: "Germania",
         text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.",
       },
       {
-        image: "img/05.jpg",
-        title:
-          "Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.",
+        image: "../img/05.jpg",
+        title: "Maldive",
+        text: "Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.",
       },
     ],
+    currentSlide = 0,
   },
 });
