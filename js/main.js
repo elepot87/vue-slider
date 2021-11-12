d9aa0cb3f314169e01c97b58c919d747feef0a61
elepot87 <78643839+elepot87@users.noreply.github.com>
2021-11-12 16:32:55 +0100

    add currentSlide

--- js/main.js
+++ js/main.js
@@ -32,6 +32,6 @@ const app = new Vue({
         text: "Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.",
       },
     ],
-    currentSlide = 0,
+    currentSlide: 0,
   },
 });
